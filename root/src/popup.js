96ca2e730b809f5acd72fb3f9891a93944b98a9b
Oscar A <oscarantonr@gmail.com>
2024-12-27 17:54:12 +0100

    Refactoring code

--- root/src/popup.js
+++ root/src/popup.js
@@ -1,4 +1,3 @@
-import { initInputComponent } from './inputComponent.js';
 let cuentaAtrasIntervalId;
 let countdownIntervalGetUp;
 
@@ -326,3 +325,40 @@ function updateCountdownStandUp(time, startAlarm) {
 
     cuentaAtrasIntervalId = setInterval(updateTimer, 1000);
 }
+
+//Increment and decrement buttons
+function decrement(e) {
+    const btn = e.target.parentNode.parentElement.querySelector(
+      'button[data-action="decrement"]'
+    );
+    const target = btn.nextElementSibling;
+    let value = Number(target.value);
+    value = Math.max(1, value - 1);
+    target.value = value;
+  }
+  
+  function increment(e) {
+    const btn = e.target.parentNode.parentElement.querySelector(
+      'button[data-action="decrement"]'
+    );
+    const target = btn.nextElementSibling;
+    let value = Number(target.value);
+    value = Math.min(120, value + 1);
+    target.value = value;
+  }
+  
+  function initInputComponent() {
+    const decrementButtons = document.querySelectorAll('button[data-action="decrement"]');
+    const incrementButtons = document.querySelectorAll('button[data-action="increment"]');
+  
+    decrementButtons.forEach(btn => {
+        btn.addEventListener("click", decrement);
+    });
+  
+    incrementButtons.forEach(btn => {
+        btn.addEventListener("click", increment);
+    });
+  }
+  
+  export { initInputComponent };
+  
